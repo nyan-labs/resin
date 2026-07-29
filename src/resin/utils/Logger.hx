756ffc94f3d309f966227232867fece77f2e5636
qzipp <154724479+qzipp@users.noreply.github.com>
2026-07-30 00:21:19 +0300

    feat: logger "improvements" (will need cleanup)

--- src/resin/utils/Logger.hx
+++ src/resin/utils/Logger.hx
@@ -1,24 +1,24 @@
 package resin.utils;
 
 final class Logger {
-  // add posinfo
-  inline static function template(text: String, what: String)
-    return '\x1B[38;2;252;160;30mresin\x1B[0m::$what $text';
+  // ill clean this up later.. sorry!
+  inline static function template(text: String, what: String, ?pos:haxe.PosInfos)
+    return '\x1B[38;2;160;250;0m\x1B[2m@ ${if(pos != null) pos.fileName + ":" + pos.lineNumber}\x1B[0m\n\x1B[38;2;252;160;30mresin\x1B[0m::$what $text';
 
   static var print = Sys.println; 
 
-  public static function warning(content: String)
+  public static function warning(content: String, ?pos:haxe.PosInfos)
     print(
-      template(content, '\x1B[1;33mwarning\x1B[0m')
+      template(content, '\x1B[1;33mwarning\x1B[0m', pos)
     );
 
-  public static function info(content: String)
+  public static function info(content: String, ?pos:haxe.PosInfos)
     print(
-      template(content, '\x1B[1;36minfo\x1B[0m')
+      template(content, '\x1B[1;36minfo\x1B[0m', pos)
     );
 
-  public static function error(content: String)
+  public static function error(content: String, ?pos:haxe.PosInfos)
     print(
-      template(content, '\\x1B[1;31merror\x1B[0m')
+      template(content, '\\x1B[1;31merror\x1B[0m', pos)
     );
 }
