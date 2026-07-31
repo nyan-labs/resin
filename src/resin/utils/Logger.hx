3b7063905505925974a25e6cfbd1d6e8f3ae26e2
hackx2 <80620538+hackx2@users.noreply.github.com>
2026-07-31 21:59:43 +0100

    refactor: `Logger`

--- src/resin/utils/Logger.hx
+++ src/resin/utils/Logger.hx
@@ -1,24 +1,24 @@
 package resin.utils;
 
+import haxe.PosInfos;
+import plasma.Renderer;
+
 final class Logger {
-  // ill clean this up later.. sorry!
-  inline static function template(text: String, what: String, ?pos:haxe.PosInfos)
-    return '\x1B[38;2;160;250;0m\x1B[2m@ ${if(pos != null) pos.fileName + ":" + pos.lineNumber}\x1B[0m\n\x1B[38;2;252;160;30mresin\x1B[0m::$what $text';
+  inline static function template(text: String, what: String, ?pos: PosInfos) {
+    final pos = pos != null ? pos.fileName + ":" + pos.lineNumber : "";
+    final position = Renderer.render('@ $pos', [Dim, FgRgb(160, 250, 0)]);
+    final resin_tag = Renderer.render("resin", [FgRgb(252, 160, 30)]);
+    return '$position\n$resin_tag::$what $text';
+  }
 
-  static var print = Sys.println; 
+  static var print = Sys.println;
 
-  public static function warning(content: String, ?pos:haxe.PosInfos)
-    print(
-      template(content, '\x1B[1;33mwarning\x1B[0m', pos)
-    );
+  public static function warning(content: String, ?pos: PosInfos)
+    print(template(content, Renderer.render("warning", [Bold, FgBasic(3)]), pos));
 
-  public static function info(content: String, ?pos:haxe.PosInfos)
-    print(
-      template(content, '\x1B[1;36minfo\x1B[0m', pos)
-    );
+  public static function info(content: String, ?pos: PosInfos)
+    print(template(content, Renderer.render("info", [Bold, FgBasic(6)]), pos));
 
-  public static function error(content: String, ?pos:haxe.PosInfos)
-    print(
-      template(content, '\\x1B[1;31merror\x1B[0m', pos)
-    );
+  public static function error(content: String, ?pos: PosInfos)
+    print(template(content, Renderer.render("error", [Bold, FgBasic(1)]), pos));
 }
