5e9fafda0eaf27e5f01d92e1d938fc6405b7f3c9
qzipp <154724479+qzipp@users.noreply.github.com>
2026-08-01 12:35:02 +0300

    feat: Mixins are working!

--- tests/ExampleMod_fabric_26_2/src/client/haxe/com/example/mixin/ExampleClientMixin.hx
+++ tests/ExampleMod_fabric_26_2/src/client/haxe/com/example/mixin/ExampleClientMixin.hx
@@ -1,5 +1,20 @@
 package com.example.mixin;
 
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@:native("net.minecraft.client.Minecraft")
+extern class Minecraft {
+
+}
+
+
+@:strict(org.spongepowered.asm.mixin.Mixin({
+  value: [Minecraft]
+}))
+@:nativeGen
 class ExampleClientMixin {
-  
+	@:meta(org.spongepowered.asm.mixin.injection.Inject(at = org.spongepowered.asm.mixin.injection.At(value = "HEAD"), method = "run"))
+	private function init(info: CallbackInfo): Void {
+    trace("Minecraft client mixin in Haxe is working!");
+  }
 }
--- tests/ExampleMod_fabric_26_2/src/main/haxe/com/example/mixin/ExampleMixin.hx
+++ tests/ExampleMod_fabric_26_2/src/main/haxe/com/example/mixin/ExampleMixin.hx
@@ -1,13 +1,15 @@
 package com.example.mixin;
 
+import java.NativeArray;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-// issue: this isn't transformed as @Mixin(server.MinecraftServer.class), currently its @Mixin(server.MinecraftServer)
-@:meta(org.spongepowered.asm.mixin.Mixin(value = net.minecraft.server.MinecraftServer))
+@:strict(org.spongepowered.asm.mixin.Mixin({
+  value: [net.minecraft.server.MinecraftServer]
+}))
+@:nativeGen
 class ExampleMixin {
-
 	@:meta(org.spongepowered.asm.mixin.injection.Inject(at = org.spongepowered.asm.mixin.injection.At(value = "HEAD"), method = "loadLevel"))
 	private function init(info: CallbackInfo): Void {
-    trace("hi world");
+    trace("Minecraft server mixin in Haxe is working!");
   }
 }
